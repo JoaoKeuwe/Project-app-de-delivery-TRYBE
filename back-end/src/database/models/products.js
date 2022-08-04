9ebfeff93d8a8ec2af1be951754cc4deae14598d
itaji-create <itaji.1999@gmail.com>
2022-08-04 18:26:06 -0300

    feat: cria e popula db com sequelize

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Products.init({
+    name: DataTypes.STRING,
+    price: DataTypes.STRING,
+    urlImage: DataTypes.STRING,
+  }, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'Products',
+  });
+  return Products;
+};
