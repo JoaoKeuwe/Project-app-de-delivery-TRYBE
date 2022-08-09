4679ad9343bb4598a591c4fa2696692bcd037d5f
JoaoKeuwe <joaov2872@hotmail.com>
2022-08-09 18:39:40 -0300

    feat: :green_heart: Comentando middlewares

--- back-end/src/routes/register.route.js
+++ back-end/src/routes/register.route.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const mid = require('../middlewares/errors');
+// const mid = require('../middlewares/errors');
 const { register } = require('../controllers/user.controller');
 
 router
-  .post('/', mid.register, register);
+  .post('/', register);
 
 module.exports = router;
