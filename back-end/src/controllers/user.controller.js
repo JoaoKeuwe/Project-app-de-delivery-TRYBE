601023f8eed2fa199a4d5b0e6766a6e99ca0793b
itaji-create <itaji.1999@gmail.com>
2022-08-14 00:30:37 -0300

    feat: adiciona images e rota static images

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -1,9 +1,9 @@
-const loginService = require('../services/user.services');
+const userServices = require('../services/user.services');
 
 const login = async (req, res) => {
   try {
-    const user = await loginService.login(req.body);
-    const token = await loginService.createToken(req.body);
+    const token = await userServices.createToken(req.body);
+    const user = await userServices.login(req.body);
     return res.status(200).json({ user, token });
   } catch (error) {
     return res.status(404).json({ message: error.message });
